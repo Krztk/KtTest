92003c5d783a4e8d7d5f04cac7806157436f1dc5
Krztk <52879665+Krztk@users.noreply.github.com>
2020-10-01 00:13:30 +0200

    Add missing migration

---
+++ KtTest/Infrastructure/Data/Migrations/20200930220852_renameChoiceProperty.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KtTest.Infrastructure.Data.Migrations
+{
+    public partial class renameChoiceProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Choice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Choice",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Choice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Choice",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
