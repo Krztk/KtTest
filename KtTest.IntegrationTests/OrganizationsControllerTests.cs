e05927c58c7c8d5ebdf8c51d29743378158846bf
Krztk <52879665+Krztk@users.noreply.github.com>
2021-03-16 12:27:18 +0100

    Add OrganizationsController integration tests

---
+++ KtTest.IntegrationTests/OrganizationsControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using KtTest.Dtos.Organizations;
+using KtTest.Infrastructure.Mappers;
+using KtTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KtTest.IntegrationTests
+{
+    [Collection(nameof(BaseFixture))]
+    public class OrganizationsControllerTests
+    {
+        private readonly BaseFixture fixture;
+
+        public OrganizationsControllerTests(BaseFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task ShouldGetOrganizationMembers()
+        {
+            var organizationOwner = fixture.UserId;
+            var response = await fixture.client.GetAsync("organizations/members");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var members = fixture.Deserialize<List<UserDto>>(responseJson);
+            var mapper = new OrganizationServiceMapper();
+            var memberDtos = fixture.OrganizationOwnerMembers[organizationOwner].Select(mapper.MapToUserDto);
+
+            members.Should().BeEquivalentTo(memberDtos);
+        }
+
+        [Theory]
+        [InlineData("testTeacher@example.com", true)]
+        [InlineData("testStudent@example.com", false)]
+        public async Task ShouldInviteNewMember(string email, bool isTeacher)
+        {
+            var dto = new InviteUserDto
+            {
+                Email = email,
+                IsTeacher = isTeacher,
+            };
+
+            var json = fixture.Serialize(dto);
+            var response = await fixture.client.PostAsync($"organizations/invite", new StringContent(json, Encoding.UTF8, "application/json"));
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var invitation = await fixture.ExecuteDbContext(db => db.Invitations.Where(x => x.Email == email).FirstOrDefaultAsync());
+            invitation.InvitedBy.Should().Be(fixture.UserId);
+            invitation.IsTeacher.Should().Be(isTeacher);
+        }
+
+        [Fact]
+        public async Task ShouldGetInvitations()
+        {
+            var invitations = new List<Invitation>
+            {
+                new Invitation
+                {
+                    Code = Guid.NewGuid().ToString(),
+                    Date = new DateTime(2021, 3, 3, 14, 0, 5, DateTimeKind.Utc),
+                    Email = "testEmail1@example.com",
+                    IsTeacher = false,
+                    InvitedBy = fixture.UserId   
+                },
+                new Invitation
+                {
+                    Code = Guid.NewGuid().ToString(),
+                    Date = new DateTime(2021, 3, 3, 15, 25, 3, DateTimeKind.Utc),
+                    Email = "testEmail2@example.com",
+                    IsTeacher = true,
+                    InvitedBy = fixture.UserId
+                },
+            };
+            
+            await fixture.ExecuteDbContext(db =>
+            {
+                db.Invitations.AddRange(invitations);
+                return db.SaveChangesAsync();
+            });
+
+            var mapper = new OrganizationServiceMapper();
+            var expectedDtos = invitations.Select(mapper.MapToInvitationDto);
+
+            var response = await fixture.client.GetAsync("organizations/invitations");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var retunedInvitationDtos = fixture.Deserialize<InvitationDto[]>(responseJson);
+            retunedInvitationDtos.Should().NotBeEmpty();
+            foreach (var expectedDto in expectedDtos)
+                retunedInvitationDtos.Should().ContainEquivalentOf(expectedDto);
+
+        }
+    }
+}
